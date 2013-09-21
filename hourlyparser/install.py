18058d94d982c7fedb52f99cdd7b21fe79166b1e
christopherfricke <christopher.fricke@bmoregeo.com>
2013-09-21 12:40:02 -0400

    modify create table

--- hourlyparser/install.py
+++ hourlyparser/install.py
@@ -6,20 +6,19 @@ import settings
 from models.Observation import Observation
 
 
-Base = declarative_base()
-
-
 def create():
 
     # create a connection to a sqlite database
     # turn echo on to see the auto-generated SQL
     engine = create_engine(settings.db_instance, echo=True)
 
+    Base = declarative_base()
+
     # get a handle on the table object
     users_table = Observation.__table__
-    # get a handle on the metadata
-    metadata = Base.metadata
-    metadata.create_all(engine)
+
+    Observation.create(engine)
+    #Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     create()
