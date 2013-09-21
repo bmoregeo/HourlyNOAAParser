e0a9e324244f607cd298eb4a7ea406eb7f1e51c9
christopherfricke <christopher.fricke@bmoregeo.com>
2013-09-21 12:47:00 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HourlyNOAAParser',
+    version='',
+    packages=['hourlyparser', 'hourlyparser.models'],
+    url='',
+    license='',
+    author='christopherfricke',
+    author_email='christopher.fricke@bmoregeo.com',
+    description=''
+)
